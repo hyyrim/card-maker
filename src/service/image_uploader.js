4bf79e4328745595af4736231fa06b7904917219
hyerim <hyyrim@gmail.com>
2022-01-11 21:52:26 +0900

    modify api key

--- src/service/image_uploader.js
+++ src/service/image_uploader.js
@@ -1,10 +1,13 @@
 class ImageUploader {
 	async upload(file) {
-		const url = 'https://api.cloudinary.com/v1_1/dxb9bkfau/image/upload';
+		const url = `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUDINARY_API_KEY}/image/upload`;
 		const formData = new FormData();
 
 		formData.append('file', file);
-		formData.append('upload_preset', 'nycdrvyj');
+		formData.append(
+			'upload_preset',
+			process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET
+		);
 
 		const result = await fetch(url, {
 			method: 'POST',
