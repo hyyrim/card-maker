02b9f35439cae373e20141e95dfaf7b4b7d849ca
hyerim <hyyrim@gmail.com>
2022-01-13 12:15:42 +0900

    use firebase realtime database - sync cards

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -6,51 +6,31 @@ import Header from '../header/header';
 import Preview from '../preview/preview';
 import styles from './maker.module.css';
 
-const Maker = ({ FileInput, authService }) => {
-	const [cards, setCards] = useState({
-		1: {
-			id: '1',
-			name: 'Ellie1',
-			company: 'Samsung',
-			theme: 'dark',
-			title: 'Software Engineer',
-			email: 'ellie@gmail.com',
-			message: 'go for it',
-			fileName: null,
-			fileURL: null,
-		},
-		2: {
-			id: '2',
-			name: 'Ellie2',
-			company: 'Samsung',
-			theme: 'light',
-			title: 'Software Engineer',
-			email: 'ellie@gmail.com',
-			message: 'go for it',
-			fileName: null,
-			fileURL: null,
-		},
-		3: {
-			id: '3',
-			name: 'Ellie3',
-			company: 'Samsung',
-			theme: 'colorful',
-			title: 'Software Engineer',
-			email: 'ellie@gmail.com',
-			message: 'go for it',
-			fileName: null,
-			fileURL: null,
-		},
-	});
-
+const Maker = ({ FileInput, authService, cardRepository }) => {
 	const history = useHistory();
+	const historyState = history?.location?.state;
+	const [cards, setCards] = useState({});
+	const [userId, setUserId] = useState(historyState && historyState.id);
+
 	const onLogout = () => {
 		authService.logout();
 	};
 
 	useEffect(() => {
+		if (!userId) {
+			return;
+		}
+		const stopSync = cardRepository.syncCards(userId, (cards) => {
+			setCards(cards);
+		});
+		return () => stopSync();
+	}, [userId]);
+
+	useEffect(() => {
 		authService.onAuthChange((user) => {
-			if (!user) {
+			if (user) {
+				setUserId(user.uid);
+			} else {
 				history.push('/');
 			}
 		});
@@ -62,6 +42,7 @@ const Maker = ({ FileInput, authService }) => {
 			updated[card.id] = card;
 			return updated;
 		});
+		cardRepository.saveCard(userId, card);
 	};
 
 	const deleteCard = (card) => {
@@ -70,6 +51,7 @@ const Maker = ({ FileInput, authService }) => {
 			delete updated[card.id];
 			return updated;
 		});
+		cardRepository.removeCard(userId, card);
 	};
 
 	return (
