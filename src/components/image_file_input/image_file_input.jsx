66efb01f8d21122daff38191685ee95f2ca3c316
hyerim <hyyrim@gmail.com>
2022-01-13 14:12:20 +0900

    performance improvement - add memo / useCallback

--- src/components/image_file_input/image_file_input.jsx
+++ src/components/image_file_input/image_file_input.jsx
@@ -1,8 +1,8 @@
 import { upload } from '@testing-library/user-event/dist/upload';
-import React, { useRef, useState } from 'react';
+import React, { memo, useRef, useState } from 'react';
 import styles from './image_file_input.module.css';
 
-const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
+const ImageFileInput = memo(({ imageUploader, name, onFileChange }) => {
 	const [loading, setLoading] = useState(false);
 	const inputRef = useRef();
 	const onButtonClick = (event) => {
@@ -41,6 +41,6 @@ const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
 			{loading && <div className={styles.loading}></div>}
 		</div>
 	);
-};
+});
 
 export default ImageFileInput;
